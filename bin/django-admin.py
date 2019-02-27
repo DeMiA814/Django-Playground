d014f797ea34bf9b0a960255ea800f5a7236b4f5
yusaku <astro.yu@icloud.com>
2019-02-27 18:56:35 +0900

    create Django app

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/umekiyusaku/PycharmProjects/django_heroku_test_again/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
