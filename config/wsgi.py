203adb9de2b77b0ae18a61caca5d5b6cc127ddcb
yusaku <astro.yu@icloud.com>
2019-02-27 19:09:24 +0900

    create Django app

--- config/wsgi.py
+++ config/wsgi.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
+from dj_static import Cling #文を新しく記述する
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application()) #文を変更する
