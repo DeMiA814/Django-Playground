66833b9ad96d4d743ac0dce248a76f0e629dfb09
AkaDeMiA13 <48004363+AkaDeMiA13@users.noreply.github.com>
2019-03-03 16:01:40 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -118,9 +118,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
-
-
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 db_from_env = dj_database_url.config()
@@ -135,7 +132,7 @@ ALLOWED_HOSTS = ['*']
 # Static asset configuration
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = '../ecom/static'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
