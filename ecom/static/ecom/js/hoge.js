4ec4a028315f58874677c7189ff789d28b691e12
AkaDeMiA13 <48004363+AkaDeMiA13@users.noreply.github.com>
2019-03-03 22:26:57 +0900

    Update hoge.js

--- ecom/static/ecom/js/hoge.js
+++ ecom/static/ecom/js/hoge.js
@@ -431,5 +431,5 @@ function createTweenScrubber(tween, seekSpeed) {
 }
 
 var canvas = document.getElementById("canvas");
-canvas.width  = 100%;
-canvas.height = 100%;
+canvas.width  = 48;
+canvas.height = 32;
