9491394412aeb3e09bd0956c762662418e6270c6
AkaDeMiA13 <48004363+AkaDeMiA13@users.noreply.github.com>
2019-03-04 00:29:17 +0900

    Update hoge.js

--- ecom/static/ecom/js/hoge.js
+++ ecom/static/ecom/js/hoge.js
@@ -12,8 +12,8 @@ function init() {
   root.renderer.setPixelRatio(window.devicePixelRatio || 1);
   root.camera.position.set(0, 0, 60);
 
-  var width = 40;
-  var height = 30;
+  var width = 200;
+  var height = 150;
 
   var slide = new Slide(width, height, 'out');
     var l1 = new THREE.ImageLoader();
