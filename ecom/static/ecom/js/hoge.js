cd01e7ac1d5a048dfd611032f91728f08aa175f5
AkaDeMiA13 <48004363+AkaDeMiA13@users.noreply.github.com>
2019-03-03 21:53:56 +0900

    Update hoge.js

--- ecom/static/ecom/js/hoge.js
+++ ecom/static/ecom/js/hoge.js
@@ -26,7 +26,7 @@ function init() {
   var slide2 = new Slide(width, height, 'in');
   var l2 = new THREE.ImageLoader();
     l2.setCrossOrigin('Anonymous');
-    l2.load('../../static/ecom/img/albert-einstein-219675_1280.jpg', function(img) {
+    l2.load('../../static/ecom/img/matrix-3109378_1920.jpg', function(img) {
         slide2.setImage(img);
     })
     
