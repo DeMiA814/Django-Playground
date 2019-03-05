27d6da5eebebcd7337f1d230ea673e8b3a4ca5f2
AkaDeMiA13 <48004363+AkaDeMiA13@users.noreply.github.com>
2019-03-05 17:06:39 +0900

    Update routers.py

--- routers.py
+++ routers.py
@@ -1,15 +1,15 @@
 class Router(object):
     def db_for_read(self, model, **hints):
         if model._meta.app_label == "akademia_sns":
-            return "default"
+            return "db2"
         elif model._meta.app_label == "ecom":
-            return "default"
+            return "db2"
  
     def db_for_write(self, model, **hints):
         if model._meta.app_label == "akademia_sns":
-            return "default"
+            return "db2"
         elif model._meta.app_label == "ecom":
-            return "default"
+            return "db2"
  
     def allow_relation(self, obj1, obj2, **hints):
         if obj1._meta.app_label == "akademia_sns" and obj2._meta.app_label == "ecom":
@@ -20,6 +20,6 @@ class Router(object):
  
     def allow_migrate(self, db, app_label, model=None, **hints):
         if app_label == "akademia_sns":
-            return db == "default"
+            return db == "db2"
         else:
-            return db == "default"
+            return db == "db2"
