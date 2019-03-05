f61d5accd4a4ffa41bd6bf0ceaafb70f7e494d33
AkaDeMiA13 <48004363+AkaDeMiA13@users.noreply.github.com>
2019-03-05 13:45:28 +0900

    Update routers.py

--- routers.py
+++ routers.py
@@ -2,12 +2,12 @@ class Router(object):
     def db_for_read(self, model, **hints):
         #ここでランダムに選択すると、dbの負荷分散っぽいことが出来る
         #例：return random.choice['default','users']
-        if model._meta.app_label == "user":
-            return "users"
+        if model._meta.app_label == "akademia_sns":
+            return "another"
         return "default"
     def db_for_write(self, model, **hints):
-        if model._meta.app_label == "user":
-            return "users"
+        if model._meta.app_label == "akademia_sns":
+            return "another"
         return "default"
     def allow_relation(self, obj1, obj2, **hints):
         #Trueはリレーションを認める、Falseは認めない、Noneはこのコードでは関知しないという意味
@@ -15,8 +15,8 @@ class Router(object):
     def allow_migrate(self, db, model):
         #userアプリの場合はusers設定のデータベースへmigrate。
         #他のアプリはusers以外のデータベースへmigrateする設定
-        if model._meta.app_label == "user"
-            return db == "users":
+        if model._meta.app_label == "akademia_sns"
+            return db == "another":
         else:
             return True
         return False
