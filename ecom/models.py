802220e7b7d7b023a66b322e2e676809bf0c5067
AkaDeMiA13 <48004363+AkaDeMiA13@users.noreply.github.com>
2019-03-05 13:22:57 +0900

    Update models.py

--- ecom/models.py
+++ ecom/models.py
@@ -1,5 +1,10 @@
 from django.db import models
-
+ 
+class Db1Table1(models.Model):
+    column1 = models.CharField(max_length=10)
+ 
+    def __str__(self):
+        return self.column1
 
 #アカウント情報
 class Person(models.Model):
