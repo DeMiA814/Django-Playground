ba5dd0f34cd5836f6db8abbfc05531b28e585719
AkaDeMiA13 <48004363+AkaDeMiA13@users.noreply.github.com>
2019-03-05 16:58:25 +0900

    Update routers.py

--- routers.py
+++ routers.py
@@ -1,22 +1,25 @@
 class Router(object):
     def db_for_read(self, model, **hints):
-        #ここでランダムに選択すると、dbの負荷分散っぽいことが出来る
-        #例：return random.choice['default','users']
         if model._meta.app_label == "akademia_sns":
             return "db2"
-        return "default"
+        elif model._meta.app_label == "ecom":
+            return None
+ 
     def db_for_write(self, model, **hints):
         if model._meta.app_label == "akademia_sns":
             return "db2"
-        return "default"
+        elif model._meta.app_label == "ecom":
+            return None
+ 
     def allow_relation(self, obj1, obj2, **hints):
-        #Trueはリレーションを認める、Falseは認めない、Noneはこのコードでは関知しないという意味
-        return None
-    def allow_migrate(self, db, model):
-        #userアプリの場合はusers設定のデータベースへmigrate。
-        #他のアプリはusers以外のデータベースへmigrateする設定
-        if model._meta.app_label == "akademia_sns"
-            return db == "db2":
-        else:
+        if obj1._meta.app_label == "akademia_sns" and obj2._meta.app_label == "ecom":
             return True
-        return False
+        elif "akademia_sns" not in [obj1._meta.app_label, obj2._meta.app_label]:
+            return True
+        return None
+ 
+    def allow_migrate(self, db, app_label, model=None, **hints):
+        if app_label == "akademia_sns":
+            return db == "db2"
+        else:
+            return db == "default"
