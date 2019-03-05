cf9372432fcfc1f4e56358095c3fe9597288f13d
AkaDeMiA13 <48004363+AkaDeMiA13@users.noreply.github.com>
2019-03-05 13:24:11 +0900

    Update models.py

--- akademia_sns/models.py
+++ akademia_sns/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Db2Table1(models.Model):
+    column1 = models.CharField(max_length=10)
+ 
+    def __str__(self):
+        return self.column1
+
 # Messageクラス
 class Message(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, \
