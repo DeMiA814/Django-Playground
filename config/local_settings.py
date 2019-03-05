04a06ca5375b7ea27c5f5acc30e9f8235bf7faaa
AkaDeMiA13 <48004363+AkaDeMiA13@users.noreply.github.com>
2019-03-05 12:44:35 +0900

    Update local_settings.py

--- config/local_settings.py
+++ config/local_settings.py
@@ -6,6 +6,10 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    'another': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db2.sqlite3'),
+    }
 }
 
 DEBUG = True
