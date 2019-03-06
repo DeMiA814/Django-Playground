49c61773563e49578aad865a8c4e764f4e5bbcfc
AkaDeMiA13 <48004363+AkaDeMiA13@users.noreply.github.com>
2019-03-06 11:40:08 +0900

    Add files via upload

---
+++ ecom/Router.py
@@ -0,0 +1,43 @@
+# DB router for app1
+
+class ecomRouter(object):
+    """
+    A router to control app1 db operations
+    """
+    def db_for_read(self, model, **hints):
+        "Point all operations on app1 models to 'db_app1'"
+        from django.conf import settings
+        if not settings.DATABASES.has_key('default'):
+            return None
+        if model._meta.app_label == 'ecom':
+            return 'default'
+        return None
+
+    def db_for_write(self, model, **hints):
+        "Point all operations on app1 models to 'db_app1'"
+        from django.conf import settings
+        if not settings.DATABASES.has_key('default'):
+            return None
+        if model._meta.app_label == 'ecom':
+            return 'default'
+        return None
+
+    def allow_relation(self, obj1, obj2, **hints):
+        "Allow any relation if a model in app1 is involved"
+        from django.conf import settings
+        if not settings.DATABASES.has_key('default'):
+            return None
+        if obj1._meta.app_label == 'ecom' or obj2._meta.app_label == 'ecom':
+            return True
+        return None
+
+    def allow_syncdb(self, db, model):
+        "Make sure the app1 app only appears on the 'app1' db"
+        from django.conf import settings
+        if not settings.DATABASES.has_key('default'):
+            return None
+        if db == 'default':
+            return model._meta.app_label == 'ecom'
+        elif model._meta.app_label == 'ecom':
+            return False
+        return None
