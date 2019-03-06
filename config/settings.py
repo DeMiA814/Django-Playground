c9ecca4d2b28bdd5c2c932669223ac6afeb9746f
AkaDeMiA13 <48004363+AkaDeMiA13@users.noreply.github.com>
2019-03-06 12:04:43 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -80,13 +80,14 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
+
     """
     'db2': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db2.sqlite3'),
     }
     """
-}
 
 print(os.path.join(BASE_DIR, 'db.sqlite3'))
 
