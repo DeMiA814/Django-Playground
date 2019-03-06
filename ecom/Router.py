8a2d1c7c6b7aba1f41f0030d0d6d0f6aa5e2f306
AkaDeMiA13 <48004363+AkaDeMiA13@users.noreply.github.com>
2019-03-06 12:27:20 +0900

    Update Router.py

--- ecom/Router.py
+++ ecom/Router.py
@@ -7,7 +7,7 @@ class ecomRouter(object):
     def db_for_read(self, model, **hints):
         "Point all operations on app1 models to 'db_app1'"
         from django.conf import settings
-        if not settings.DATABASES.has_key('default'):
+        if 'default' not in settings.DATABASES:
             return None
         if model._meta.app_label == 'ecom':
             return 'default'
@@ -16,7 +16,7 @@ class ecomRouter(object):
     def db_for_write(self, model, **hints):
         "Point all operations on app1 models to 'db_app1'"
         from django.conf import settings
-        if not settings.DATABASES.has_key('default'):
+        if 'default' not in settings.DATABASES:
             return None
         if model._meta.app_label == 'ecom':
             return 'default'
@@ -25,7 +25,7 @@ class ecomRouter(object):
     def allow_relation(self, obj1, obj2, **hints):
         "Allow any relation if a model in app1 is involved"
         from django.conf import settings
-        if not settings.DATABASES.has_key('default'):
+        if 'default' not in settings.DATABASES:
             return None
         if obj1._meta.app_label == 'ecom' or obj2._meta.app_label == 'ecom':
             return True
@@ -34,7 +34,7 @@ class ecomRouter(object):
     def allow_syncdb(self, db, model):
         "Make sure the app1 app only appears on the 'app1' db"
         from django.conf import settings
-        if not settings.DATABASES.has_key('default'):
+        if 'default' not in settings.DATABASES:
             return None
         if db == 'default':
             return model._meta.app_label == 'ecom'
