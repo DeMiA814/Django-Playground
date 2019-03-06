5ed1a894c6ddb23ce8f4a93ab035018dea65492d
AkaDeMiA13 <48004363+AkaDeMiA13@users.noreply.github.com>
2019-03-06 12:10:37 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -76,26 +76,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-
-
 """
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db',
+        'NAME': '/app/db.sqlite3',
     },
     'db2': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db2',
+        'NAME': '/app/db2.sqlite3',
     },
 }
 """
