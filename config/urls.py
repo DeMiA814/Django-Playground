5a75c8d80270139115e95907d1e7e0f1fefd893c
AkaDeMiA13 <48004363+AkaDeMiA13@users.noreply.github.com>
2019-03-07 17:36:09 +0900

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ecom/', include('ecom.urls')),
+    path('akademia_sns/', include('akademia_sns.urls')),
 ]
