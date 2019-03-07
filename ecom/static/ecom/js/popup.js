7da2d4733aa0cc9f32524ceeef943dc425c9870c
AkaDeMiA13 <48004363+AkaDeMiA13@users.noreply.github.com>
2019-03-07 17:25:43 +0900

    Delete popup.js

--- ecom/static/ecom/js/popup.js
+++
@@ -1,9 +0,0 @@
-//appends an "active" class to .popup and .popup-content when the "Open" button is clicked
-$(".open").on("click", function(){
-  $(".popup-overlay, .popup-content").addClass("active");
-});
-
-//removes the "active" class to .popup and .popup-content when the "Close" button is clicked
-$(".close, .popup-overlay").on("click", function(){
-  $(".popup-overlay, .popup-content").removeClass("active");
-});
