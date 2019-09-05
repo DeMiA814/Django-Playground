bb75b3d590f82c33351a10dd25fa3c1fba70042a
takumi <taku_kaji@ezweb.ne.jp>
2019-09-05 17:50:10 +0900

    productimage

--- config/settings.py
+++ config/settings.py
@@ -137,7 +137,7 @@ ALLOWED_HOSTS = ['*']
 # Static asset configuration
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = '../ecom/static'
+STATIC_ROOT = '../ecom/static/'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
